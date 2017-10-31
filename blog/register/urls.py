87c428e8b8dbafe405f71a8e238ed910909dc81b
Dmitry <dmitry.bl.5967@gmail.com>
2017-10-31 13:54:31 +0300

    blog_project

--- blog/register/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from register import views
-
-urlpatterns = [
-    url(r'^register/', views.register, name='register')
-]
